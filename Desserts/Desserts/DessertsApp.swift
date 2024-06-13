4b9eb9fba86f9a62657f12b90d597363b6f0ac0f
Logan Catt <loganparme97@gmail.com>
2024-06-13 13:29:06 -0600

    Initial project commit

---
+++ Desserts/Desserts/DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsApp.swift
+//  Desserts
+//
+//  Created by Logan Parmeter on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
